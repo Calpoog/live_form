ba6b4faa46b2fa4bec2efbdb90909a684b4f976c
goodmc4 <goodmc4@nationwide.com>
2023-02-02 13:33:46 -0500

    Add passthroughs to switch

--- lib/src/widgets/live_switch_form_field.dart
+++ lib/src/widgets/live_switch_form_field.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart' hide FormFieldState;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:live_form/live_form.dart';
@@ -5,9 +6,47 @@ import 'package:live_form/live_form.dart';
 class LiveSwitchFormField extends StatelessWidget {
   final ToggleFormControl control;
 
+  // passthrough values
+  final Color? activeColor;
+  final Color? activeTrackColor;
+  final Color? inactiveThumbColor;
+  final Color? inactiveTrackColor;
+  final ImageProvider? activeThumbImage;
+  final ImageErrorListener? onActiveThumbImageError;
+  final ImageProvider? inactiveThumbImage;
+  final ImageErrorListener? onInactiveThumbImageError;
+  final MaterialStateProperty<Color?>? thumbColor;
+  final MaterialStateProperty<Color?>? trackColor;
+  final MaterialStateProperty<Icon?>? thumbIcon;
+  final MaterialTapTargetSize? materialTapTargetSize;
+  final DragStartBehavior dragStartBehavior;
+  final MouseCursor? mouseCursor;
+  final Color? focusColor;
+  final Color? hoverColor;
+  final MaterialStateProperty<Color?>? overlayColor;
+  final double? splashRadius;
+
   const LiveSwitchFormField({
     super.key,
     required this.control,
+    this.activeColor,
+    this.activeTrackColor,
+    this.inactiveThumbColor,
+    this.inactiveTrackColor,
+    this.activeThumbImage,
+    this.onActiveThumbImageError,
+    this.inactiveThumbImage,
+    this.onInactiveThumbImageError,
+    this.thumbColor,
+    this.trackColor,
+    this.thumbIcon,
+    this.materialTapTargetSize,
+    this.dragStartBehavior = DragStartBehavior.start,
+    this.mouseCursor,
+    this.focusColor,
+    this.hoverColor,
+    this.overlayColor,
+    this.splashRadius,
   });
 
   @override
@@ -23,6 +62,24 @@ class LiveSwitchFormField extends StatelessWidget {
         return Switch(
           value: control.value,
           onChanged: (value) => control.update(value),
+          activeColor: activeColor,
+          activeTrackColor: activeTrackColor,
+          inactiveThumbColor: inactiveThumbColor,
+          inactiveTrackColor: inactiveTrackColor,
+          activeThumbImage: activeThumbImage,
+          onActiveThumbImageError: onActiveThumbImageError,
+          inactiveThumbImage: inactiveThumbImage,
+          onInactiveThumbImageError: onInactiveThumbImageError,
+          thumbColor: thumbColor,
+          trackColor: trackColor,
+          thumbIcon: thumbIcon,
+          materialTapTargetSize: materialTapTargetSize,
+          dragStartBehavior: dragStartBehavior,
+          mouseCursor: mouseCursor,
+          focusColor: focusColor,
+          hoverColor: hoverColor,
+          overlayColor: overlayColor,
+          splashRadius: splashRadius,
         );
       },
     );
