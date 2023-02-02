5b163fd9b191598993988df1121e2c60337c95d1
goodmc4 <goodmc4@nationwide.com>
2023-02-02 13:09:57 -0500

    add passthroughs to live checkbox

--- lib/src/widgets/live_checkbox_form_field.dart
+++ lib/src/widgets/live_checkbox_form_field.dart
@@ -5,9 +5,41 @@ import 'package:live_form/live_form.dart';
 class LiveCheckboxFormField extends StatelessWidget {
   final ToggleFormControl control;
 
+  // pass-through values
+  final MouseCursor? mouseCursor;
+  final Color? activeColor;
+  final MaterialStateProperty<Color?>? fillColor;
+  final Color? checkColor;
+  // final bool tristate;
+  final MaterialTapTargetSize? materialTapTargetSize;
+  final VisualDensity? visualDensity;
+  final Color? focusColor;
+  final Color? hoverColor;
+  final MaterialStateProperty<Color?>? overlayColor;
+  final double? splashRadius;
+  final FocusNode? focusNode;
+  final bool autofocus;
+  final OutlinedBorder? shape;
+  final BorderSide? side;
+
   const LiveCheckboxFormField({
     super.key,
     required this.control,
+    // this.tristate = false,
+    this.mouseCursor,
+    this.activeColor,
+    this.fillColor,
+    this.checkColor,
+    this.focusColor,
+    this.hoverColor,
+    this.overlayColor,
+    this.splashRadius,
+    this.materialTapTargetSize,
+    this.visualDensity,
+    this.focusNode,
+    this.autofocus = false,
+    this.shape,
+    this.side,
   });
 
   @override
@@ -23,6 +55,21 @@ class LiveCheckboxFormField extends StatelessWidget {
         return Checkbox(
           value: control.value,
           onChanged: (value) => control.update(value ?? false),
+          isError: control.invalid && LiveForm().shouldShowError(control.state),
+          mouseCursor: mouseCursor,
+          activeColor: activeColor,
+          fillColor: fillColor,
+          checkColor: checkColor,
+          focusColor: focusColor,
+          hoverColor: hoverColor,
+          overlayColor: overlayColor,
+          splashRadius: splashRadius,
+          materialTapTargetSize: materialTapTargetSize,
+          visualDensity: visualDensity,
+          focusNode: focusNode,
+          autofocus: autofocus,
+          shape: shape,
+          side: side,
         );
       },
     );
